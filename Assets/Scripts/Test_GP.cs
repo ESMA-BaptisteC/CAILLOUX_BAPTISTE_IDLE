ee21bc73b599975ebbe0f4215c50c9de0a418d8b
ESMA-BaptisteC <b.cailloux@rns.ecolescreatives.com>
2024-11-22 16:53:57 +0100

    END SESSION COMMIT

--- Assets/Scripts/Test_GP.cs
+++ Assets/Scripts/Test_GP.cs
@@ -1,11 +1,17 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class Test_GP : MonoBehaviour
 {
     [SerializeField]
-    private int _wordsWritten, _maxWords, _score;
+    private TextMeshProUGUI _textDoubleChances;
+    
+    [SerializeField]
+    private int _currentWords, _maxWords, _currentPages, _maxPages, _books;
+
     [SerializeField]
     private int  _typeValue = 1, _doubleChances;
 
@@ -13,44 +19,62 @@ public class Test_GP : MonoBehaviour
     private void Start()
     {
         _maxWords = Random.Range(10, 100);
+        _maxPages = Random.Range(10, 100);
     }
 
     private void Update()
     {
-
+        _textDoubleChances.text = _doubleChances.ToString();
 
         Type(_typeValue);
 
-        if (_wordsWritten >= _maxWords)
+        if (_currentWords >= _maxWords)
         {
             NewPage();
+        }
+
+        if (_currentPages >= _maxPages)
+        {
+            NewBook();
         }
     }
 
     private void Type(int value)
     {
-        if (Input.anyKeyDown)
+        if (Input.anyKeyDown && !Input.GetMouseButtonDown(0))
         {
-            _wordsWritten += value;
+            _currentWords += value;
 
-            int tempValue = Random.Range(0, 100);
-            if (tempValue <= _doubleChances)
-            {
-                _wordsWritten += value;
-            }
+            DoubleChances(value);
         }
     }
 
-    private void NewPage()
-    {
-        _score++;
-        _wordsWritten = 0;
-        _maxWords = Random.Range(10, 100);
-    }
 
     public void UpgradeDoubleChances()
     {
         _doubleChances++;
     }
 
+    private void DoubleChances(int value)
+    {
+        int tempValue = Random.Range(0, 100);
+        if (tempValue <= _doubleChances)
+        {
+            _currentWords += value;
+        }
+    }
+
+    private void NewBook()
+    {
+        _books++;
+        _currentPages = 0;
+        _maxPages = Random.Range(10, 100);
+    }
+
+    private void NewPage()
+    {
+        _currentPages++;
+        _currentWords = 0;
+        _maxWords = Random.Range(10, 100);
+    }
 }
